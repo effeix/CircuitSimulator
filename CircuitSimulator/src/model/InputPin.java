9316815d9348ada50199865403666b9fc79af087
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2016-03-31 11:34:18 -0300

    DONE: AndGate, OrGate, NotGate, InputPin

--- CircuitSimulator/src/model/InputPin.java
+++ CircuitSimulator/src/model/InputPin.java
@@ -1,5 +1,14 @@
 package model;
 
 public class InputPin {
+	Source source;
+	int index;
 
+	public Source getSource() {
+		return source;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
 }
--- CircuitSimulator/src/model/OrGate.java
+++ CircuitSimulator/src/model/OrGate.java
@@ -1,5 +1,13 @@
 package model;
 
 public class OrGate extends LogicGate {
-
+	private InputPin pinA;
+	private InputPin pinB;
+	
+	@Override
+	public boolean getOutputValue(int index) {
+		boolean A = pinA.getSource().getOutputValue(pinA.getIndex());
+		boolean B = pinB.getSource().getOutputValue(pinB.getIndex());
+		return A || B;
+	}
 }
