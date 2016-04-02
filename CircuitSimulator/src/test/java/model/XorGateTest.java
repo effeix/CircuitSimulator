f94a4654c6aa9cee0c45eae7f7219edd8f676754
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2016-04-02 19:20:29 -0300

    DONE: XorGateTest

--- CircuitSimulator/src/test/java/model/XorGateTest.java
+++ CircuitSimulator/src/test/java/model/XorGateTest.java
@@ -2,13 +2,93 @@ package model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class XorGateTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Input00() {
+		//Since our switches objects start at false, we don't need to toggle their state to test the 0 x 0 case
+		
+		//Components for the circuit
+		XorGate xor = new XorGate();
+		Switch switch1 = new Switch();
+		Switch switch2 = new Switch();
+		
+		//Create and set input objects
+		InputPin pinA = new InputPin();
+		pinA.setSource(switch1);
+		InputPin pinB = new InputPin();
+		pinB.setSource(switch2);
+		xor.setPin(pinA, pinB);
+		
+		//Test if output if false
+		Assert.assertFalse(xor.getOutputValue(0));
+	}
+	
+	@Test
+	public void Input01() {
+		//Components for the circuit
+		XorGate xor = new XorGate();
+		Switch switch1 = new Switch();
+		Switch switch2 = new Switch();
+		
+		//Set input B (switch2) as 1
+		switch2.toggleState();
+		
+		//Create and set input objects
+		InputPin pinA = new InputPin();
+		pinA.setSource(switch1);
+		InputPin pinB = new InputPin();
+		pinB.setSource(switch2);
+		xor.setPin(pinA, pinB);
+				
+		//Test if the output is false
+		Assert.assertTrue(xor.getOutputValue(0));	
+	}
+	
+	@Test
+	public void Input10() {
+		//Components for the circuit
+		XorGate xor = new XorGate();
+		Switch switch1 = new Switch();
+		Switch switch2 = new Switch();
+				
+		//Set input A (switch1) as 1
+		switch1.toggleState();
+		
+		//Create and set input objects
+		InputPin pinA = new InputPin();
+		pinA.setSource(switch1);
+		InputPin pinB = new InputPin();
+		pinB.setSource(switch2);
+		xor.setPin(pinA, pinB);
+		
+		//Test if the output is false
+		Assert.assertTrue(xor.getOutputValue(0));
+	}
+	
+	@Test
+	public void Input11() {
+		//Components for the circuit
+		XorGate xor = new XorGate();
+		Switch switch1 = new Switch();
+		Switch switch2 = new Switch();
+						
+		//Set both inputs as 1
+		switch1.toggleState();
+		switch2.toggleState();
+				
+		//Create and set input objects
+		InputPin pinA = new InputPin();
+		pinA.setSource(switch1);
+		InputPin pinB = new InputPin();
+		pinB.setSource(switch2);
+		xor.setPin(pinA, pinB);
+				
+		//Test if the output is true
+		Assert.assertFalse(xor.getOutputValue(0));
 	}
 
 }
