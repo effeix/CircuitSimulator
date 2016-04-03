8abc725e341abb8702a552cd6ba413d825fc1a15
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2016-04-02 21:16:17 -0300

    DONE: FullAdderGate

--- CircuitSimulator/src/main/java/model/FullAdderGate.java
+++ CircuitSimulator/src/main/java/model/FullAdderGate.java
@@ -15,51 +15,51 @@ public class FullAdderGate extends LogicGate {
 	@Override
 	public boolean getOutputValue(int index) {
 
-	boolean[] out = new boolean[2];
+		boolean[] out = new boolean[2];
 
-	//criando as portas lógicas
+		//criando as portas lógicas
 
-	XorGate xor1 = new XorGate();
-	XorGate xor2 = new XorGate();
-	AndGate and1 = new AndGate();
-	AndGate and2 = new AndGate();
-	OrGate or = new OrGate();
+		XorGate xor1 = new XorGate();
+		XorGate xor2 = new XorGate();
+		AndGate and1 = new AndGate();
+		AndGate and2 = new AndGate();
+		OrGate or = new OrGate();
 
-	//Xor1
+		//Xor1
 
-	xor1.setPin(faPinA,faPinB);
-	InputPin outXor1 = new InputPin();
-	outXor1.setSource(xor1);
+		xor1.setPin(faPinA,faPinB);
+		InputPin outXor1 = new InputPin();
+		outXor1.setSource(xor1);
 
-	//Xor2
+		//Xor2
 
-	xor2.setPin(outXor1,faPinC);
-	InputPin outXor2 = new InputPin();
-	outXor2.setSource(xor2);
+		xor2.setPin(outXor1,faPinC);
+		InputPin outXor2 = new InputPin();
+		outXor2.setSource(xor2);
 
-	//And1
+		//And1
 
-	and1.setPin(faPinC,outXor1);
-	InputPin outAnd1 = new InputPin();
-	outAnd1.setSource(and1);
+		and1.setPin(faPinC,outXor1);
+		InputPin outAnd1 = new InputPin();
+		outAnd1.setSource(and1);
 
-	//And2
+		//And2
 
-	and2.setPin(faPinB,faPinA);
-	InputPin outAnd2 = new InputPin();
-	outAnd2.setSource(and2);
+		and2.setPin(faPinB,faPinA);
+		InputPin outAnd2 = new InputPin();
+		outAnd2.setSource(and2);
 
-	//Or
+		//Or
 
-	or.setPin(outAnd1,outAnd2);
-	InputPin outOr = new InputPin();
-	outOr.setSource(or);
+		or.setPin(outAnd1,outAnd2);
+		InputPin outOr = new InputPin();
+		outOr.setSource(or);
 
-	//array de saídas
+		//array de saídas
 
-	out[0] = xor2.getOutputValue(0);
-	out[1] = or.getOutputValue(0);
+		out[0] = xor2.getOutputValue(0);
+		out[1] = or.getOutputValue(0);
 
-	return out[index];
+		return out[index];
 	}
 }
