cd6d31080300db60269c082b580c1085903a2102
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2016-04-04 09:00:25 -0300

    Update MainWindow

--- CircuitSimulator/src/main/java/view/MainWindow.java
+++ CircuitSimulator/src/main/java/view/MainWindow.java
@@ -3,9 +3,10 @@ package view;
 import java.awt.*;
 import javax.swing.*;
 
-public class MainWindow {
-	private static int height = 400;
-	private static int width = 750;
+public class MainWindow extends JPanel{
+	private final static int HEIGHT = 400;
+	private final static int WIDTH = 750;
+	
 	public static void main(String[] args) {
 		createWindow();
 	}
@@ -14,11 +15,19 @@ public class MainWindow {
 		JFrame frame = new JFrame("Circuit Simulator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.setPreferredSize(new Dimension(width,height));
+		frame.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		frame.add(new MainWindow());
 		
 		//SETTINGS
 		frame.setLocationRelativeTo(null);
 		frame.pack();
 		frame.setVisible(true);
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.LIGHT_GRAY);
+		g.fillRect(0, 0, WIDTH, HEIGHT); //PosX, PosY, width, height
+		g.setColor(Color.BLACK);
+		g.drawLine(10, 10, 50, 50);
 	}
 }
